cfc9ee4e0c2d31080161e9c6701e0d3e8f8df5d3
Aallam <1042932843@qq.com>
2019-05-22 10:26:36 +0800

    Initial commit

---
+++ desktop/src/com/dusky/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dusky.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dusky.game.duskyworld;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new duskyworld(), config);
+	}
+}
