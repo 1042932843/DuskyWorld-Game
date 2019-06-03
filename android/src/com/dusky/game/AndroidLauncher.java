9cb226158a35f562b7ab49ace7324a6a56de20b0
Aallam <1042932843@qq.com>
2019-06-03 17:19:12 +0800

    尝试性的编写抛射游戏

--- android/src/com/dusky/game/AndroidLauncher.java
+++ android/src/com/dusky/game/AndroidLauncher.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.dusky.game.test.NuttyGame;
 import com.dusky.game.test.TestGame;
 
 public class AndroidLauncher extends AndroidApplication {
@@ -11,6 +12,6 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new DuskyWorld(), config);
+		initialize(new NuttyGame(), config);
 	}
 }
